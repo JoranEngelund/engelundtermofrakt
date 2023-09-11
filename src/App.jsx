0b830efa6fe52231f538a599200f3be78a21ef0b
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2023-09-11 18:19:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Switch, Redirect } from "react-router-dom";
+import { Route, Routes, Redirect } from "react-router-dom";
 import Layout from "./components/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/pages/Home";
@@ -19,15 +19,13 @@ import Contact from "./components/pages/Contact";
 export default function App() {
   return (
     <Routes>
-      <Switch>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="om-oss" element={<About />} />
-          <Route path="kontakt" element={<Contact />} />
-          <Route path="*" element={<h1>Page not found</h1>} />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="om-oss" element={<About />} />
+        <Route path="kontakt" element={<Contact />} />
+        <Route path="*" element={<h1>Page not found</h1>} />
+      </Route>
+      <Redirect to="/" />
     </Routes>
   );
 }
