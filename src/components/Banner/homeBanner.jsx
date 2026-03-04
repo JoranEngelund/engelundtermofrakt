8240bf4ac73e138f899702752909482e2c0865c8
JoranEngelund <joranengelund@hotmail.com>
2026-03-04 16:36:34 +0100

    Update homeBanner.jsx

--- src/components/Banner/homeBanner.jsx
+++ src/components/Banner/homeBanner.jsx
@@ -8,7 +8,8 @@ export default function HomeBanner() {
   const media = data?._embedded?.["wp:featuredmedia"]?.[0];
   const bannerImage = media?.source_url;
   const bannerAltText = media?.alt_text || "Engelund Termofrakt";
-  const title = data?.title?.rendered || "Temperaturkontrollert transport";
+  const title =
+    data?.title?.rendered || "Intet lass for lite, Intet lass for stort";
   const subtitle = "Pålitelig logistikk siden 1996";
 
   return (
